851cb4224816f678ba8ebde561b183069fae2ae2
HenryLulu <why318why@gmail.com>
2019-01-01 23:31:19 +0800

    add about

--- src/page/about/About.js
+++ src/page/about/About.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import {Route, Link} from "react-router-dom";
 
+import './About.less';
+
 export default class extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            m1page: 1
+        };
+    }
+
     render() {
         return (
-            <div className="about" style={{marginTop: '1rem'}}>
-                
+            <div className="about">
+                <div className="wrapper m1">
+                    {this.state.m1page === 1 && (
+                        <div className="p1">
+                            <h1 className="title">尝试+兴趣=前端</h1>
+                            <h1 className="title"></h1>
+                        </div>
+                    )}
+                </div>
             </div>
         );
     }
