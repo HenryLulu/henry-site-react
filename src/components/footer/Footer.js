dcb41d99f6925034369ce1d8aa6910b09bedf763
HenryLulu <why318why@gmail.com>
2019-01-28 11:09:31 +0800

    new logo

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,8 +6,9 @@ export default () => (
         <p className="top" onClick={() => {window.scrollTo(0, 0)}}>
             <i className="anticon icon-up-square-o"></i>
         </p>
+        <div class="logo"></div>
         <p className="more">CONNECT WITH ME</p>
-        <p className="email">Email: why318why@163.com</p>
+        <p className="email">Email: why318why@gmail.com</p>
         <div className="icons">
             <a href="https://github.com/HenryLulu" target="_blank">
                 <i className="anticon icon-github"></i>
