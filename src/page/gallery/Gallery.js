567ff82e5a0e152be08b4274c7299b5a5cfd0e94
HenryLulu <why318why@gmail.com>
2019-03-15 16:25:02 +0800

    fix: safari img

--- src/page/gallery/Gallery.js
+++ src/page/gallery/Gallery.js
@@ -5,19 +5,19 @@ import './Gallery.less';
 
 const images = [
     {
-        src: 'https://user-images.githubusercontent.com/14797054/54413911-63370d80-4732-11e9-960b-99b0343d4181.png',
+        src: 'https://user-images.githubusercontent.com/14797054/54417962-7fd94280-473e-11e9-8d5b-1081b1e7ece1.png',
         tag: '2013 - 宣传海报 - 北科大',
         title: '索思科技协会招新海报'
     }, {
-        src: 'https://user-images.githubusercontent.com/14797054/54413923-7053fc80-4732-11e9-8c2f-cb0a15e8c4cc.png',
+        src: 'https://user-images.githubusercontent.com/14797054/54417977-8cf63180-473e-11e9-9a51-a2c65ad07352.png',
         tag: '2012 - 手册封面 - 北科大',
         title: '《2012新生攻略》封面'
     }, {
-        src: 'https://user-images.githubusercontent.com/14797054/54413904-57e3e200-4732-11e9-8daa-f285cb7cf612.png',
+        src: 'https://user-images.githubusercontent.com/14797054/54417950-79e36180-473e-11e9-8406-d9adf3b15fe0.png',
         tag: '2012 - 产品周边 - iBeiKe',
         title: 'iBeiKe 11周年站庆文化衫（销量No.1）'
     }, {
-        src: 'https://user-images.githubusercontent.com/14797054/54413934-78ac3780-4732-11e9-991b-6843aa18567e.png',
+        src: 'https://user-images.githubusercontent.com/14797054/54417980-92ec1280-473e-11e9-8af1-fdfacc745b81.png',
         tag: '2013 - 宣传海报 - iBeiKe',
         title: 'iBeike 12周年站庆海报'
     }
@@ -35,9 +35,11 @@ export default class extends React.Component {
             let $img = new Image();
             $img.src = image.src;
             $img.data = image;
-            $img.height = 100;
             $img.onload = e => {
                 loadCount ++;
+                $img.ratio = $img.width / $img.height;
+                $img.width = 0;
+                $img.height = 0;
                 pool.push($img);
                 let $imgbox = $(`
                     <div class="img-box">
@@ -68,12 +70,11 @@ export default class extends React.Component {
 const setImgWidth = pool => {
     const vw = window.innerWidth;
     const w = vw > 750 ? vw - 35 : vw;
-    const totalImgWidth = pool.map($img => $img.width).reduce((s, a) => s + a);
-    const ratio = w / totalImgWidth;
+    const totalRatio = pool.map($img => $img.ratio).reduce((s, a) => s + a);
+    console.log(totalRatio)
     for (let $img of pool) {
-        const {width, height} = $img;
-        $img.width = ratio * width;
-        $img.height = ratio * height;
+        $img.height = w / totalRatio;
+        $img.width = $img.height * $img.ratio;
         if ($img.width > 750) {
             $img.height = $img.height / $img.width * 500;
             $img.width = 500;
