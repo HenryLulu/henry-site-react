ac7f1121d23ea2b8040af359066590483a587010
henrywanghongyu <henrywanghongyu@didiglobal.com>
2019-10-31 19:31:01 +0800

    up

---
+++ build/precache-manifest.4ed1a519afad1c22d1d826f89cb6ced6.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "da5487970dad35d0fb7d9f671ced7506",
+    "url": "/static/media/dd.da548797.png"
+  },
+  {
+    "revision": "16636b1b4efcc2be5c36",
+    "url": "/static/css/main.7ee2dde7.chunk.css"
+  },
+  {
+    "revision": "b337fc8c27901d12dafb",
+    "url": "/static/js/1.b337fc8c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e3691829fb2b147b5a914c121fc9c2c3",
+    "url": "/static/media/logo_gold.e3691829.svg"
+  },
+  {
+    "revision": "aa74699967129534eb49fd070f49c5cd",
+    "url": "/static/media/tj.aa746999.png"
+  },
+  {
+    "revision": "16636b1b4efcc2be5c36",
+    "url": "/static/js/main.16636b1b.chunk.js"
+  },
+  {
+    "revision": "04549e82be8fc4ac8926c22c6c148fb8",
+    "url": "/static/media/bg.04549e82.jpg"
+  },
+  {
+    "revision": "7aa49b144d2bdd1418ec3de26ebdbde8",
+    "url": "/static/media/DINCond-Bold.7aa49b14.otf"
+  },
+  {
+    "revision": "9ec5e40edddff9ff300e6791941fe869",
+    "url": "/static/media/iconfont.9ec5e40e.eot"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/static/media/iconfont.3a2ba315.ttf"
+  },
+  {
+    "revision": "ca5d4588dad9c32f49e895e7f19479df",
+    "url": "/static/media/iconfont.ca5d4588.svg"
+  },
+  {
+    "revision": "83b24565f53c0791225c7ba81cd1939e",
+    "url": "/index.html"
+  }
+];
